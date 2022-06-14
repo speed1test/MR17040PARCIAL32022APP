b2d453455666cd106c1c1543aef76e56f7498374
speed01test <edwin12sv@gmail.com>
2022-06-13 22:21:36 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mr17040parcial3pdm1152022app/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mr17040parcial3pdm1152022app.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
